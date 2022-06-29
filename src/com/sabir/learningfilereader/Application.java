e58ffdf5c4311525ed0da6f25e20372783d61b1a
Sab1rz <stha_sabir@live.com>
2022-06-29 17:01:03 -0500

    version 2

--- src/com/sabir/learningfilereader/Application.java
+++ src/com/sabir/learningfilereader/Application.java
@@ -1,2 +1,20 @@
-package com.sabir.learningfilereader;public class Application {
+package com.sabir.learningfilereader;
+
+import java.io.*;
+
+public class Application {
+    public static void main(String[] args) {
+        File file=new File("myfile.txt");
+        try {
+            FileReader fileReader=new FileReader(file);
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+            String line=bufferedReader.readLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Unable to tread the file");
+            System.out.println("There are many");
+        }
+
+    }
 }
